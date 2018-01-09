89471cd67a1f6344de3f156636ad0191ea283b5b
Steve Heyes <steve@substrakt.com>
2018-01-09 16:17:36 +0000

    ADD: Add a new playlist

--- app.rb
+++ app.rb
@@ -10,7 +10,22 @@ get "/" do
  erb :"playlist/index"
 end
 
-get "/new" do
+post "/" do
+  if params[:playlist].has_key? :url
+    @playlist = Playlist.new(params[:playlist])
+    if @playlist.save
+      @title = 'Guess Whos Playlist'
+      @playlists = Playlist.all
+      erb :"playlist/index"
+    else
+      redirect "create"
+    end
+  else
+    redirect "create"
+  end
+end
+
+get "/create" do
   @title = 'Add your playlist - Guess Whos Playlist'
-  erb :"playlist/new"
+  erb :"playlist/create"
 end
